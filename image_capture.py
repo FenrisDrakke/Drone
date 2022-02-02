afb6a81155f9c5da689177b465db7c4c655eb751
Bence_Hordos <hordos.bence@gmail.com>
2022-02-02 22:21:41 +0100

    keyboard input & control finished

--- image_capture.py
+++ image_capture.py
@@ -1,18 +1,18 @@
 from djitellopy import tello
 import cv2
 
-me = tello.Tello()
+drone = tello.Tello()
 # connect take care of the communication
-me.connect()
-print(me.get_battery())
+drone.connect()
+print(drone.get_battery())
 
-me.streamon()
+drone.streamon()
 
 while True:
-      image = me.get_frame_read().frame
-      # resize the image for faster and more stable stream
-      Image = cv2.resize(image,(360,240))
-      # imshow creates a window
-      cv2.imshow("Drones image", image)
-      # keeps open the window
-      cv2.waitkey(1)
+    image = drone.get_frame_read().frame
+    # resize the image for faster and more stable stream
+    Image = cv2.resize(image,(360,240))
+    # imshow creates a window
+    cv2.imshow("Stream", image)
+    # keeps open the window
+    cv2.waitKey(1)
